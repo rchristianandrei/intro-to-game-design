e2b3cee9cdaa42b92d2b103a7f86d18345e06405
rchristian <98152145+rchristianandrei@users.noreply.github.com>
2022-09-19 13:04:54 +0800

    polished OE2

--- OE2/main.py
+++ OE2/main.py
@@ -1,44 +1,50 @@
 import pygame
 
-pygame.init()
 
-window = pygame.display.set_mode((800, 600))
-playing = True
+def move_shape(key, speed):
+    if key:
+        return speed
+    return 0
 
-pygame.display.set_caption("First Pygame")
 
-x, y, width, height, speed = 415, 275, 50, 50, 0.3
+def main():
+    pygame.init()
 
-x2, y2, width2, height2, speed2 = 335, 275, 50, 50, 0.3
+    window = pygame.display.set_mode((800, 600))
+    playing = True
 
-while playing:
-    window.fill((0, 0, 0))
+    pygame.display.set_caption("First Pygame")
 
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            playing = False
+    x, y, width, height, speed, color = 415, 275, 50, 50, 0.3, (255, 255, 0)
+    x2, y2, width2, height2, speed2, color2 = 335, 275, 50, 50, 0.3, (0, 255, 0)
 
-    keys = pygame.key.get_pressed()
+    while playing:
+        window.fill((0, 0, 0))
 
-    if keys[pygame.K_LEFT]:
-        x -= speed
-    if keys[pygame.K_RIGHT]:
-        x += speed
-    if keys[pygame.K_UP]:
-        y -= speed
-    if keys[pygame.K_DOWN]:
-        y += speed
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                playing = False
 
-    if keys[pygame.K_a]:
-        x2 -= speed
-    if keys[pygame.K_d]:
-        x2 += speed
-    if keys[pygame.K_w]:
-        y2 -= speed
-    if keys[pygame.K_s]:
-        y2 += speed
+        keys = pygame.key.get_pressed()
 
-    pygame.draw.rect(window, (0, 255, 0), (x2, y2, width2, height2))
-    pygame.draw.rect(window, (255, 255, 0), (x, y, width, height))
-    pygame.display.update()
+        # Move Objects
+        x += move_shape(keys[pygame.K_LEFT], -speed)
+        x += move_shape(keys[pygame.K_RIGHT], speed)
+        y += move_shape(keys[pygame.K_UP], -speed)
+        y += move_shape(keys[pygame.K_DOWN], speed)
 
+        x2 += move_shape(keys[pygame.K_a], -speed)
+        x2 += move_shape(keys[pygame.K_d], speed)
+        y2 += move_shape(keys[pygame.K_w], -speed)
+        y2 += move_shape(keys[pygame.K_s], speed)
+
+        # Draw Objects
+        pygame.draw.rect(window, color, (x, y, width, height))
+        pygame.draw.rect(window, color2, (x2, y2, width2, height2))
+
+        pygame.display.update()
+
+    pygame.quit()
+
+
+main()
