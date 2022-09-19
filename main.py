7c5bc1c2cbcd41f2c8eaaf1e8c07dadd31cbe1ed
rchristian <98152145+rchristianandrei@users.noreply.github.com>
2022-09-19 11:59:51 +0800

    Initial Commit

---
+++ main.py
@@ -0,0 +1,12 @@
+import pygame
+
+pygame.init()
+
+window = pygame.display.set_mode((800, 600))
+playing = True
+
+pygame.display.set_caption("First Pygame")
+
+while playing:
+    pygame.display.update()
+
