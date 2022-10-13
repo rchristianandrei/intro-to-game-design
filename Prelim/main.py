a32c935e86bcd7ce3aeff40d2f33a7691e214a81
rchristian <98152145+rchristianandrei@users.noreply.github.com>
2022-10-13 18:51:32 +0800

    added another object

--- Prelim/main.py
+++ Prelim/main.py
@@ -31,6 +31,13 @@ def main():
     object1.w = 77
     object1.h = 146
 
+    object2 = Object(415, 275)
+    object2.idle_scale = (117, 141)
+    object2.run_scale = (110, 146)
+    object2.jump_scale = (110, 140)
+    object2.w = 77
+    object2.h = 146
+
     # Assign Sprite Settings
     for x in range(10):
         object1.IDLE.append(load(f'../images/object1/Idle/Idle__00{x}.png'))
@@ -42,6 +49,17 @@ def main():
         object1.JUMP.append(load(f'../images/object1/Jump/Jump__00{x}.png'))
 
     object1.resize_sprites()
+
+    for x in range(10):
+        object2.IDLE.append(load(f'../images/object2/Idle/Idle ({x+1}).png'))
+
+    for x in range(10):
+        object2.RUN.append(load(f'../images/object2/Run/Run ({x+1}).png'))
+
+    for x in range(10):
+        object2.JUMP.append(load(f'../images/object2/Jump/Jump ({x+1}).png'))
+
+    object2.resize_sprites()
 
     while playing:
         window.fill((0, 0, 0))
@@ -60,12 +78,20 @@ def main():
             keys[pygame.K_a],
             keys[pygame.K_d],
             keys[pygame.K_SPACE]]
+        object2.moving = [
+            keys[pygame.K_UP],
+            keys[pygame.K_DOWN],
+            keys[pygame.K_LEFT],
+            keys[pygame.K_RIGHT],
+            keys[pygame.K_KP_0]]
 
         # Move Objects
         object1.move()
+        object2.move()
 
         # Draw Objects
         window.blit(object1.active_sprite, (object1.x, object1.y, object1.w, object1.h))
+        window.blit(object2.active_sprite, (object2.x, object2.y, object2.w, object2.h))
 
         # Update
         pygame.display.update()
